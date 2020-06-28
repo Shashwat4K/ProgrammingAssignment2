8efc6bafadb675bd46c0b5f4abf678477306e533
Shashwat4K <shashwatkdm@gmail.com>
2020-06-28 11:30:05 +0530

    Completed the code stubs, and tested the working

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# Auxiliary function which calculates and returns inverse of a matrix
+calculateInverse <- function(mat) {
+    # Using solve() to calculate matrix inverse
+    # First check if the determinant is 0
+    if(nrow(mat) != ncol(mat)) {
+        return(NaN) # Return NaN if the matrix is not square.
+    } else if(det(mat) == 0) {
+        return(NaN)  # Return NaN if determinant is 0. 
+                     # As the matrix is not invertible
+    }
+    solve(mat)
 }
 
+# By referring the example, I've written an equivalent code
+# for making a cache matrix. Here, the only difference was,
+# that we have to store 'matrix inverse' rather than 'mean'
+makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL # inverse of the matrix 'x'
+    
+    # setter method to assign values to 'x' and 'inv'
+    # We can change the value of 'x' here
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    
+    # getter method
+    # Returns the matrix 'x' stored
+    get <- function() x
+    
+    # sets the 'inv' variable the value: 'myinv'
+    setInverse <- function(myinv) inv <<- myinv 
+    
+    # gets (returns) the computed inverse 'inv'
+    getInverse <- function() inv
+    
+    # bind all these functions to a list
+    # Equivalent to creation on a Java object
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
 
-## Write a short comment describing this function
-
+# Function
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getInverse()
+    # Check whether inverse is precomputed,
+    # if yes, then directly return that
+    if(is.null(inverse) == FALSE) {
+        message("Inverse is already calculated, returning the data")
+        return(inverse)
+    }
+    # Otherwise...
+    else {
+        # since the inverse wasn't already cached, 
+        # we need to calculate it from scratch
+        mat <- x$get()
+        inverse <- calculateInverse(mat)
+        x$setInverse(inverse)
+        return(inverse)
+    }
 }
